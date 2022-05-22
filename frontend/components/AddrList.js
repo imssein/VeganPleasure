ce165ef6cb50155a45b00d65598aab515af1372a
sein <hyuntpdls@gmail.com>
2022-05-23 01:45:24 +0900

    feat: useState로 행정동 데이터 받아오기

---
+++ frontend/components/AddrList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function AddrList({ address }) {
+  return (
+    <div>{address.region_2depth_name}</div>
+  )
+}
+
+export default AddrList;
--- frontend/components/Geolocation.js
+++ frontend/components/Geolocation.js
@@ -1,39 +1,42 @@
-import React, {useEffect} from 'react';
-import Axios from 'axios';
-import axios from 'axios';
-
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import axios from "axios";
+import AddrList from "./AddrList";
 function Geolocation(props) {
-    
-    useEffect(() => {
-        if (navigator.geolocation) {
-            //사용자 위치를 geolocation.getCurrentPosition을 이용하여
-            // latitude와 longitude를 받아온다.
-            navigator.geolocation.getCurrentPosition((position) => {
-              let latitude = position.coords.latitude;
-              let longitude = position.coords.longitude;
-            //   console.log(latitude, longitude)
-             
+    const [address, setAddress] = useState([]);
 
-            if( longitude && latitude ){
-                axios.get(`https://dapi.kakao.com/v2/local/geo/coord2regioncode.json?x=${longitude}&y=${latitude}`, {
-                    headers: { Authorization: `KakaoAK ${process.env.NEXT_PUBLIC_REST_API_KEY}` }
-                }).then(result => {
-                    // 서울특별시 __ 구 __동 
-                    const addr_name = result.data.documents[0].address_name;
-                    // __구
-                    const region_name = result.data.documents[0].region_2depth_name;
-                    console.log(addr_name);
-                    console.log(region_name);
-                }) 
-            }
+  useEffect(() => {
+    if (navigator.geolocation) {
+      //사용자 위치를 geolocation.getCurrentPosition을 이용하여
+      // latitude와 longitude를 받아온다.
+      navigator.geolocation.getCurrentPosition((position) => {
+        let latitude = position.coords.latitude;
+        let longitude = position.coords.longitude;
+        //   console.log(latitude, longitude)
+
+        if (longitude && latitude) {
+          axios
+            .get(
+              `https://dapi.kakao.com/v2/local/geo/coord2regioncode.json?x=${longitude}&y=${latitude}`,
+              {
+                headers: {
+                  Authorization: `KakaoAK ${process.env.NEXT_PUBLIC_REST_API_KEY}`,
+                },
+              }
+            )
+            .then((result) => {
+                setAddress(result.data.documents[0]);
             });
         }
-    })
-    return (
-        <div>
-            
-        </div>
-    );
+      });
+    }
+  }, []);
+
+  return(
+      <>
+      <AddrList address={address} />
+      </>
+  )
 }
 
 export default Geolocation;
